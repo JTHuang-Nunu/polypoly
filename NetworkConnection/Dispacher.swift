f0deedab45d1aa24526ed59eee0fb6fb63bb20ed
Chengpong1127 <Chengpong1127@gmail.com>
2023-05-22 12:29:13 +0800

    implement simple sendAction

--- NetworkConnection/Dispacher.swift
+++ NetworkConnection/Dispacher.swift
@@ -9,17 +9,40 @@ import Foundation
 class Dispacher{
     
     private var sessionManager: SesstionManager
+    private var encoder: JSONEncoder
     
     init(){
         sessionManager = SesstionManager(ip: "172.20.10.5", port: 8000)
+        encoder = JSONEncoder()
     }
     
-    public func InputAction(){
+    public func InputAction(action: PlayerAction){
+        sendAction(action: action)
+    
         
     }
     
-    private func sendAction(){
+    private func sendAction(action: PlayerAction){
+        var data: String = ""
+        do{
+            let jsonData = try encoder.encode(action)
+            data = String(data: jsonData, encoding: .utf8)!
+        }
+        catch{
+            print(error)
+        }
+        var outputMessage = ["type" : "PlayerAction",
+                            "content" : data
+        ]
         
+        do{
+            let jsonData = try encoder.encode(outputMessage)
+            let jsonString = String(data: jsonData, encoding: .utf8)!
+            sessionManager.sendData(message: jsonString)
+        }
+        catch{
+            print(error)
+        }
     }
     
     
