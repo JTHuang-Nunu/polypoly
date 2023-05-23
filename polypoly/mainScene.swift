232958778e43e7234706bddac06ae375c640ac0a
JT_MAC <ge6435wr@gmail.com>
2023-05-24 00:32:55 +0800

    Update mainScene.swift

--- polypoly/mainScene.swift
+++ polypoly/mainScene.swift
@@ -7,51 +7,64 @@
 
 import SpriteKit
 import Foundation
+
 class MainScene: SKScene, SKPhysicsContactDelegate {
+    var screenCenter: CGPoint!
     
-    var ballNode: SKSpriteNode!
-    var arrowNode: SKShapeNode!
+//    var player1Node: SKSpriteNode!
+    var arrowNode: Arrow!
     var resetButton: SKShapeNode!
     var isDragging = false
     var isDrawing = false
     var startPoint: CGPoint = .zero
     
-    var circle: SKShapeNode!
+    var rect: SKShapeNode!
     var lineNode: DrawingLine!
     var linePath: CGMutablePath!
     var powerBar: PowerBar!
     var timer: Timer?
 
-    
+    var player1: CharacterModel!
+    var player2: CharacterModel!
+    var player1Node: SKSpriteNode!
+    var player2Node: SKSpriteNode!
+    struct PlayerPosition {
+        private static let offset: CGFloat = 200
+        static var p1: CGPoint {
+            let x = UIScreen.main.bounds.minX + offset
+            let y = UIScreen.main.bounds.midY
+            return CGPoint(x: x, y: y)
+        }
+        static var p2: CGPoint {
+            let x = UIScreen.main.bounds.maxX - offset
+            let y = UIScreen.main.bounds.midY
+            return CGPoint(x: x, y: y)
+        }
+    }
+
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
+        screenCenter = CGPoint(x: self.frame.midX, y: self.frame.midY)
         createScene()
         createResetButton()
         
-        circle = SKShapeNode(rect: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
-        circle.strokeColor = .black
-        circle.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        addChild(circle)
+        //create xy axis
+        rect = SKShapeNode(rect: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        rect.strokeColor = .black
+        rect.position = screenCenter
+        addChild(rect)
         
+        // create player1's Ball
+//        self.player1 = CharacterModel(ID: 999, position: screenCenter)
+        self.player1 = CharacterModel(ID: 999, position: PlayerPosition.p1)
+        self.player2 = CharacterModel(ID: 111, position: PlayerPosition.p2)
+        player1Node = player1.node
+        player2Node = player2.node
+        addChild(player1.node)
+        addChild(player2Node)
         
-        ballNode = Ball()
-        ballNode.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        addChild(ballNode)
-        
-        // createArrow
-        let arrowWidth: CGFloat = 10
-        let arrowHeight: CGFloat = 70
-        let arrowPath = CGMutablePath()
-        arrowPath.move(to: CGPoint(x: 0, y: 0))
-        arrowPath.addLine(to: CGPoint(x: arrowHeight, y: arrowWidth/2))
-        arrowPath.addLine(to: CGPoint(x: arrowHeight, y: -arrowWidth/2))
-        arrowPath.closeSubpath()
-        arrowNode = SKShapeNode(path: arrowPath)
-        arrowNode.fillColor = .black
-        //        arrowNode.strokeColor = .red
-        arrowNode.lineWidth = 2
-        arrowNode.position = CGPoint(x: frame.midX, y: ballNode.frame.midY)
-        //        arrowNode.zRotation =
+        // create Arrow
+        arrowNode = Arrow()
         addChild(arrowNode)
     }
     func didBegin(_ contact: SKPhysicsContact) {
@@ -81,35 +94,21 @@ class MainScene: SKScene, SKPhysicsContactDelegate {
         if let touch = touches.first {
             let location = touch.location(in: self)
             //ballNode is touched by finger
-            if ballNode.contains(location) {
+            if player1Node.contains(location) {
                 isDragging = true
-                startPoint = location
+                arrowNode.updateArrow(start: location)
             }
             //resetButton(red btn) is touched
             if resetButton.contains(location) {
                 powerBar.power = powerBar.maxPower
-                ballNode.removeFromParent()
-                ballNode = Ball()
-                ballNode.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-                addChild(ballNode)
+                player1Node.removeFromParent()
+                player1Node = Ball()
+                player1Node.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+                addChild(player1Node)
             }
             //scene is touched
             else{
-//                let circle = SKShapeNode(circleOfRadius: 10) // 創建圓形節點
-//                circle.position = touch.location(in: self) // 設置位置
-//                circle.strokeColor = SKColor.red // 設置邊框顏色
-//                circle.lineWidth = 2 // 設置邊框寬度
-//                circle.fillColor = SKColor.blue // 設置填充顏色
-//                circle.physicsBody = SKPhysicsBody(circleOfRadius: 10)
-//                circle.physicsBody?.usesPreciseCollisionDetection = true
-//                circle.physicsBody?.isDynamic = false
-//
-//
-//                circle.physicsBody?.categoryBitMask = PhysicsCategory.Line
-//                circle.physicsBody?.contactTestBitMask = PhysicsCategory.Ball
-//                addChild(circle) // 將節點添加到場景中
-                
-                if powerBar.power >= 10{
+                if powerBar.power >= 10{ //
                     isDrawing = true
                 }
                 
@@ -120,41 +119,24 @@ class MainScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     var lastTouchLocation: CGPoint?
-    var totalDistance: CGFloat = 0
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if isDragging {
             if let touch = touches.first {
-                // get basic info
-                let location = touch.location(in: self)
-                let distance = startPoint.distance(to: location)
-                let direction = CGVector(dx: location.x - startPoint.x, dy: location.y - startPoint.y)
-                let angle = direction.angle
-                arrowNode.zRotation = angle
-                
-                // set arrow startpoint from ball, let arrow go around the edge of ball
-                let ballRadius = ballNode.size.width/2
-                // along the circumference
-                arrowNode.position = CGPoint(x: ballNode.frame.midX + ballRadius*CGFloat(cos(angle)),y: ballNode.frame.midY + ballRadius*CGFloat(sin(angle)))
-                
-                //  arrow zomm by dragging distance
-                arrowNode.yScale = distance / 100
-                arrowNode.xScale = distance / 100
-
+                arrowNode.updateArrow(current: touch.location(in: self), objectNode: player1Node)
             }
         }
+        
         else if isDrawing{
             if let touch = touches.first {
                 //get drawing distance, then consume power
                 if let lastTouchLocation = lastTouchLocation {
-                       let currentTouchLocation = touch.location(in: self)
-                       let distance = hypot(currentTouchLocation.x - lastTouchLocation.x, currentTouchLocation.y - lastTouchLocation.y)
-//                       totalDistance += distance
+                    let currentTouchLocation = touch.location(in: self)
+                    let distance = hypot(currentTouchLocation.x - lastTouchLocation.x, currentTouchLocation.y - lastTouchLocation.y)
                     powerBar.power = powerBar.power - distance * 0.1
                 }
                 lineNode.movePath(toPoint: touch.location(in: self))
-                //record the last Location
-                lastTouchLocation = touch.location(in: self)
-//                print("drawing move")
+                
+                lastTouchLocation = touch.location(in: self) //record the last Location
                 
                 //if powerBar's power less equal 0
                 if powerBar.power <= 0{
@@ -169,10 +151,8 @@ class MainScene: SKScene, SKPhysicsContactDelegate {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if isDragging {
             isDragging = false
-            let force = arrowNode.xScale * 100
-            let direction = CGVector(dx: arrowNode.zRotation.cosine(), dy: arrowNode.zRotation.sine())
-            let impulse = CGVector(dx: -direction.dx * force, dy: -direction.dy * force)
-            ballNode.physicsBody?.applyImpulse(impulse)
+            let impulse = arrowNode.getImpulse()
+            player1Node.physicsBody?.applyImpulse(impulse)
             arrowNode.xScale = 0
         }
         else if isDrawing{
@@ -182,17 +162,15 @@ class MainScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
-    //    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        if isDragging {
-    //            isDragging = false
-    //            arrowNode.xScale = 0
-    //        }
-    //    }
+        override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+            print("Touches were Cancelled")
+        }
     
+    // Stoping the ballNode when speed less than 1
     override func update(_ currentTime: TimeInterval) {
-        if let physicsBody = ballNode.physicsBody {
+        if let physicsBody = player1Node.physicsBody {
             let speed = sqrt(physicsBody.velocity.dx*physicsBody.velocity.dx + physicsBody.velocity.dy*physicsBody.velocity.dy)
-            if speed < 1 {
+            if speed < 1 { //when speed < 1, stop movement
                 physicsBody.velocity = CGVector(dx: 0, dy: 0)
                 physicsBody.angularVelocity = 0
             }
@@ -206,19 +184,15 @@ class MainScene: SKScene, SKPhysicsContactDelegate {
     }
 
     func createScene(){
-        let bgd = SKSpriteNode(imageNamed: "marble")
-        bgd.zRotation = 1.57
-        bgd.size.width = bgd.size.width
-        bgd.size.height = bgd.size.height
-        bgd.position = CGPoint(x:self.frame.midX, y:self.frame.midY)
-        bgd.zPosition = -1
+        // set Background
+        let bgd = Background(image: "marble", position: screenCenter, rotate: 1.57)
         addChild(bgd)
 
         // set Gravity direction to Z
         physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
         
         // set Wall
-        let wall = Wall(size: self.size)
+        let wall = Wall(size: self.size,position: screenCenter)
         wall.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         addChild(wall)
         
