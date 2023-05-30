876648d2f4e74b829090202641d8483054fce66e
Justin Huang <ge6435wr@gmail.com>
2023-05-30 19:59:27 +0800

    reverse commit to charAction parent

---
+++ polypoly/PlayerInputController.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerController.swift
+//  polypoly
+//
+//  Created by Cheng Pong Huang on 2023/5/24.
+//
+
+import Foundation
+
+
+class InputController: PlayerInputController{
+    func OnDoAction(action: (PlayerAction) -> Void) {
+        // TODO: implement
+    }
+    
+    
+}
