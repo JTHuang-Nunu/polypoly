c04148bf614e67ff7fea334c50eb2442f49f64f1
Chengpong1127 <Chengpong1127@gmail.com>
2023-06-02 19:26:09 +0800

    Add DashLine

--- polypoly/Object/Pointer.swift
+++ polypoly/Object/Pointer.swift
@@ -11,16 +11,33 @@ import SpriteKit
 class Pointer: SKShapeNode{
     let arrowLength: CGFloat = 20
     let arrawAngle: CGFloat = 20 * CGFloat.pi / 180
-    init(startPoint: CGPoint, endPoint: CGPoint){
+    var startPoint: CGPoint
+    var endPoint: CGPoint
+    var startAt: CGFloat = 0
+    let dashLen: CGFloat = 5
+    let gapLen: CGFloat = 5
+    var useDashLine: Bool
+    
+    init(startPoint: CGPoint, endPoint: CGPoint, useDashLine: Bool = true){
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+        self.useDashLine = useDashLine
+        
         super.init()
         self.path = GetArrowPath(startPoint: startPoint, endPoint: endPoint)
         self.strokeColor = .blue
+        setKeepUpdateAction()
     }
     public func UpdatePointer(startPoint: CGPoint, endPoint: CGPoint){
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+        UpdatePointer()
+    }
+    private func UpdatePointer(){
         self.path = GetArrowPath(startPoint: startPoint, endPoint: endPoint)
     }
+    
     private func GetArrowPath(startPoint: CGPoint, endPoint: CGPoint) -> CGMutablePath{
-        //endPoint will be the arrow point
         let path = CGMutablePath()
         
         let lineAngle = atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x)
@@ -29,11 +46,29 @@ class Pointer: SKShapeNode{
         let arrowLeftPoint = CGPoint(x: endPoint.x - arrowLength * cos(lineAngle - arrawAngle), y: endPoint.y - arrowLength * sin(lineAngle - arrawAngle))
         let arrowRightPoint = CGPoint(x: endPoint.x - arrowLength * cos(lineAngle + arrawAngle), y: endPoint.y - arrowLength * sin(lineAngle + arrawAngle))
         path.move(to: startPoint)
-        path.addDashLine(to: arrowPoint, dashLen: 5, gapLen: 5, startAt: 0)
+        if useDashLine{
+            path.addDashLine(to: arrowPoint, dashLen: 5, gapLen: 5, startAt: startAt)
+        }
+        else{
+            path.addLine(to: arrowPoint)
+        }
         path.addLine(to: arrowLeftPoint)
         path.move(to: arrowPoint)
         path.addLine(to: arrowRightPoint)
         return path
+    }
+    private func setKeepUpdateAction(){
+        let updateAction = SKAction.run {
+            self.startAt += 0.5
+            if self.startAt > self.dashLen + self.gapLen{
+                self.startAt = 0
+            }
+            self.UpdatePointer()
+        }
+        let waitAction = SKAction.wait(forDuration: 0.01)
+        let sequenceAction = SKAction.sequence([updateAction, waitAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        self.run(repeatAction)
     }
     
     
