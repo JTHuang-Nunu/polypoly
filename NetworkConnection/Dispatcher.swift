96668378604b41fd8cb718512a4931074cb1594e
Chengpong1127 <Chengpong1127@gmail.com>
2023-06-03 11:45:16 +0800

    Refact Dispatcher

--- NetworkConnection/Dispatcher.swift
+++ NetworkConnection/Dispatcher.swift
@@ -6,32 +6,30 @@
 //
 //  Dispacher is a class that dispaches player actions to other device by SessionManager
 import Foundation
-class Dispatcher{
+class Dispatcher {
+    public let OnReceivePlayerAction = Event<PlayerAction>()
+    
+    
     private var sessionManager: SesstionManager
-    private var encoder: JSONEncoder
-    private var decoder: JSONDecoder
+    private let encoder: JSONEncoder = JSONEncoder()
+    private var decoder: JSONDecoder = JSONDecoder()
+    
     private var PlayerID: UUID
     init(){
         sessionManager = SesstionManager(ip: "localhost", port: 8000)
-        
-        encoder = JSONEncoder()
-        decoder = JSONDecoder()
+
         PlayerID = UUID()
-        sessionManager.OnReceiveData += { str in
-            let message = self.decodeJSON(jsonString: str)
-            self.handleMessage(message: message)
-        }
-        
+        sessionManager.OnReceiveData += handleReceiveMessage
     }
     
     public func RequestRoom(){
-        let data = Message(PlayerID: PlayerID, MessageType: .RequestRoom)
-        sessionManager.sendData(message: encodeJSON(message: data),reliable: true)
+        let message = Message(PlayerID: PlayerID, MessageType: .RequestRoom)
+        sendMessage(message: message)
     }
     
     public func sendAction(action: PlayerAction){
         let data = Message(PlayerID: PlayerID, MessageType: .PlayerAction, Content: encodeJSON(message: action))
-        sessionManager.sendData(message: encodeJSON(message: data),reliable: true)
+        sendMessage(message: data)
     }
     
     private func handleMessage(message: Message){
@@ -54,15 +52,43 @@ class Dispatcher{
             break
         }
     }
-    private func decodeJSON(jsonString: String)->Message{
+    
+    private func handleReceivePlayerAction(content: String){
+        let decodedAction = decodeJSON(PlayerAction.self, jsonString: content)
+        if let action = decodedAction{
+            OnReceivePlayerAction.Invoke(action)
+        }
+        else{
+            print("Error: Receive invalid player action")
+        }
+    }
+    
+    private func handleReceiveMessage(content: String){
+        let decodedMessage = decodeJSON(Message.self, jsonString: content)
+        if let message = decodedMessage{
+            handleMessage(message: message)
+        }
+        else{
+            print("Error: Receive invalid message")
+        
+        }
+    }
+    
+    
+    private func sendMessage(message: Message){
+        sessionManager.sendData(message: encodeJSON(message: message),reliable: true)
+    }
+    
+
+    private func decodeJSON<T: Codable>(_ type: T.Type, jsonString: String)->T?{
         do{
             let jsonData = jsonString.data(using: .utf8)!
-            let message = try decoder.decode(Message.self, from: jsonData)
+            let message = try decoder.decode(T.self, from: jsonData)
             return message
         }catch{
             print(error)
         }
-        return Message(PlayerID: UUID(), MessageType: .CancelRequest)
+        return nil
     
     }
     private func encodeJSON(message: Codable)-> String{
